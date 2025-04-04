7e2410ed5ec4e0b90fbc89912cbb5970bbbbd593
hangoocn <imhangoo@gmail.com>
2025-04-03 21:01:19 -0700

    initial commit

---
+++ bin/my-pipeline.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { MyPipelineStack } from '../lib/my-pipeline-stack';
+
+const app = new cdk.App();
+new MyPipelineStack(app, 'MyPipelineStack', {
+  env: {
+    account: '911836057208',
+    region: 'ca-central-1',
+  }
+});
+
+app.synth();
