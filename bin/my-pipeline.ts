dad592ce6cb92a2b59620f40f147e18b070a8d54
hangoocn <imhangoo@gmail.com>
2025-04-03 21:23:35 -0700

    add change

--- bin/my-pipeline.ts
+++ bin/my-pipeline.ts
@@ -5,7 +5,7 @@ import { MyPipelineStack } from '../lib/my-pipeline-stack';
 const app = new cdk.App();
 new MyPipelineStack(app, 'MyPipelineStack', {
   env: {
-    account: '911836057208',
+    account: '423563880681',
     region: 'ca-central-1',
   }
 });
