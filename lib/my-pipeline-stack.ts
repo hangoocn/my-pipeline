994f40daa54e8861fdf97f45db254d4ccfb694fa
hangoocn <imhangoo@gmail.com>
2025-04-03 22:15:53 -0700

    fix

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { GitHubTrigger } from 'aws-cdk-lib/aws-codepipeline-actions';
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,7 +10,10 @@ export class MyPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('git@github.com:hangoocn/my-pipeline.git', 'main'),
+        input: CodePipelineSource.gitHub('hangoocn/my-pipeline', 'main', {
+          authentication: cdk.SecretValue.secretsManager('github-token'),
+          trigger: GitHubTrigger.WEBHOOK,
+        }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
